342098a019896f38cd1a517179b4b1f1cb28d796
Rhys Balevicius <development@rhys.tech>
2023-09-04 02:59:37 +0200

    Setup iOS framework

---
+++ chronos/chronos/chronos.h
@@ -0,0 +1,18 @@
+//
+//  chronos.h
+//  chronos
+//
+//  Created by Rhys Balevicius.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for chronos.
+FOUNDATION_EXPORT double chronosVersionNumber;
+
+//! Project version string for chronos.
+FOUNDATION_EXPORT const unsigned char chronosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <chronos/PublicHeader.h>
+
+
