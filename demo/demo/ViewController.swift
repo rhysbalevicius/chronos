8af02dff38aa786896479051963908afc8a3307e
Rhys Balevicius <development@rhys.tech>
2023-10-04 16:38:25 -0400

    Setup example project

---
+++ demo/demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  demo
+//
+//  Created by hermes on 2023-10-04.
+//
+
+import UIKit
+import Chronos
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
